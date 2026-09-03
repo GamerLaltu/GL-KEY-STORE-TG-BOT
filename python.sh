21630d43151b9436b56c16994d19a193212d877b
GamerLaltu <github20092@gmail.com>
2026-09-03 09:38:15 +0530

    Add files via upload

---
+++ python.sh
@@ -0,0 +1,19 @@
+pkg update ~ pkg upgrade
+
+pkg install rust clang cmake make
+
+pip install -U pip setuptools wheel maturin
+
+pip install aiogram aiohttp
+
+python --version
+
+pip --version
+
+termux-setup-storage
+
+ls
+
+cd ~/storage/SELLING_BOT_TG
+
+python Main.py
