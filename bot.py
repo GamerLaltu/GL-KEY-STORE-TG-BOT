fae4e335871ace52bf4621072e23bd0662c354b7
GamerLaltu <github20092@gmail.com>
2026-09-04 08:17:36 +0530

    Change BOT_TOKEN in bot.py
    Updated the BOT_TOKEN for the bot configuration.

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ from aiogram.types import (
 # ==============================================================================
 # 1. BOT CONFIGURATION & CONSTANTS
 # ==============================================================================
-BOT_TOKEN = "8706432939:AAErPIiGf7YAPGTcOMQVKUhy5Dg-fAFmRHM"
+BOT_TOKEN = "8706432939:AAFzmLU4X2KOBA1-efbQUvyioVWtW-qDNYQ"
 BOT_USERNAME = "@GL_KEY_STORE_BOT"
 ADMIN_ID = 7498723155
 ADMIN_CONTACT = "@THEKINGCHEATS_8"
